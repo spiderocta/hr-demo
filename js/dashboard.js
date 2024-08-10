5f7a827eedf4ed161573fe47053f4d178fedd76f
spiderocta <muhammadahmed@tutanota.com>
2024-08-10 08:15:04 +0300

    employees  information section [a view]

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,25 @@
+// dynamic sections in the same page
 function showSection(sectionName) {
-  // Get all sections within the content div
   var sections = document.querySelectorAll('.content section');
-
-  // Iterate over each section and hide it
   sections.forEach(function(section) {
     section.style.display = 'none';
   });
-
-  // Show the selected section
   var targetSection = document.querySelector('.content .' + sectionName);
   if (targetSection) {
     targetSection.style.display = 'block';
   }
 }
+
+// frront end Table Search
+$('#search').keyup(function() {
+  var regex = new RegExp($('#search').val(), "i");
+  var rows = $('table tr:gt(0)');
+  rows.each(function (index) {
+    title = $(this).children("#title").html()
+    if (title.search(regex) != -1) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
